3fcc52707ed55a2def1c956f5afda1288c144f86
sunny kumar <sunnykumar1516@gmail.com>
2024-11-08 18:55:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import utility as ut
 def selectLanguage(EnterText,selectLanguage):
     res= ut.translateIt(EnterText,"English",selectLanguage)
     return res.content
-demo = gr.Interface(
-   
-    title="I am a language translator",
-    fn=selectLanguage,
     
-    inputs=["text",gr.Radio(["Hindi","French","Spanish"])],
-    outputs=["text"],
+with gr.Blocks() as demo:
 
-
-)
-
+    gr.Interface(
+        title="I am a language translator",
+        fn=selectLanguage,    
+        inputs=["text",gr.Radio(["Hindi","French","Spanish"])],
+        outputs=["text"],
+    )
+    gr.Markdown("<p style='color:red;'>Created By: <span style ='color:yellow'>Sunny Kumar</span>.</p>")
+    gr.Markdown("<p style='color:red;'>Contact me at: <span style ='color:yellow'>SunnyKumar1516@gmail.com</span>.</p>")
+    gr.Markdown("<p style='color:red;'>Linkdin: <span style ='color:yellow'>https://www.linkedin.com/in/sunny-kumar-b232417a/</span>.</p>")
+    
 demo.launch()
